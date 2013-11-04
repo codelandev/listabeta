292e7c90ede82832330496318453b89431b40b09
Patrick Müller <patrick.e.muller@gmail.com>
2013-11-04 18:22:36 -0200

    Add HIGHLIGHT to Startups

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131031182043) do
+ActiveRecord::Schema.define(version: 20131104191742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,28 +50,29 @@ ActiveRecord::Schema.define(version: 20131031182043) do
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "startups", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "name",                   default: "", null: false
-    t.string   "city",                   default: "", null: false
-    t.string   "pitch",                  default: "", null: false
-    t.string   "state",                  default: "", null: false
-    t.string   "website",                default: "", null: false
-    t.string   "twitter",                default: "", null: false
-    t.string   "markets",                default: "", null: false
-    t.integer  "status",                 default: 0,  null: false
-    t.text     "description",            default: "", null: false
-    t.string   "screenshot",             default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "name",                   default: "",    null: false
+    t.string   "city",                   default: "",    null: false
+    t.string   "pitch",                  default: "",    null: false
+    t.string   "state",                  default: "",    null: false
+    t.string   "website",                default: "",    null: false
+    t.string   "twitter",                default: "",    null: false
+    t.string   "markets",                default: "",    null: false
+    t.integer  "status",                 default: 0,     null: false
+    t.text     "description",            default: "",    null: false
+    t.string   "screenshot",             default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "highlighted",            default: false
   end
 
   add_index "startups", ["email"], name: "index_startups_on_email", unique: true, using: :btree
