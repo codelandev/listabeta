81d96b4f49627fb5ad7ebcd2baed281ce9ea5f41
Patrick Müller <patrick.e.muller@gmail.com>
2013-11-07 18:10:17 -0200

    Created INDEX and SHOW for Startups

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,8 +1,11 @@
 class Startup < ActiveRecord::Base
+  extend FriendlyId
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  friendly_id :name, use: :slugged
 
   acts_as_ordered_taggable
   acts_as_ordered_taggable_on :markets
@@ -15,10 +18,10 @@ class Startup < ActiveRecord::Base
             presence: true
   validates :website, url: true
 
-  scope :highlighteds, -> { where(highlighted: true, approved: true) }
-  scope :unhighlighteds, -> { where(highlighted: false, approved: true) }
-  scope :approvateds, -> { where(approved: true) }
-  scope :unapprovateds, -> { where(approved: false) }
+  scope :highlighteds, -> { where(highlighted: true, approved: true).order(:created_at) }
+  scope :unhighlighteds, -> { where(highlighted: false, approved: true).order(:created_at) }
+  scope :approvateds, -> { where(approved: true).order(:created_at) }
+  scope :unapprovateds, -> { where(approved: false).order(:created_at) }
 
   def highlight!
     update_column(:highlighted, true) and save
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Listabeta::Application.routes.draw do
   ActiveAdmin.routes(self)
 
   get 'mercados/(:tag)' => 'pages#markets', as: :markets
+  
+  resources :startups
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
