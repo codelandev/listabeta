2116c88f8ac7bbed3e248a5d6ac2036724c3b81e
Patrick Müller <patrick.e.muller@gmail.com>
2013-11-14 15:59:22 -0200

    Fixed some problems with questionnaires show and controller

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -1,6 +1,6 @@
 class QuestionnairesController < InheritedResources::Base
   actions :create, :new
-  belongs_to :startup, :param => :startup_id
+  belongs_to :startup, param: :startup_id
   before_filter :authenticate_startup!, only:[:index, :show]
 
   def index
@@ -9,25 +9,30 @@ class QuestionnairesController < InheritedResources::Base
 
   def new
     @questionnaire = Questionnaire.new
-    @startup = Startup.friendly.find(params[:startup_id])
+    @startup = resource
     new!
   end
 
   def create
-    params[:startup_id] = Startup.friendly.find(params[:startup_id]).id
-    
     @questionnaire = Questionnaire.new(permitted_params[:questionnaire])
-    @questionnaire.startup_id = params[:startup_id]
+    @questionnaire.startup_id = resource.id
     create!(notice: I18n.t('flash.questionnaires.send_success')) { root_path }
   end
 
   def show
-    if resource.startup_id != current_startup.id
+    @questionnaire = Questionnaire.find(params[:id])
+    if @questionnaire.startup != current_startup
       redirect_to startup_questionnaires_path, alert: I18n.t('flash.questionnaires.unauthorized_access')
+    else
+      @questionnaire
     end
   end
 
   protected
+
+  def resource
+    @startup ||= Startup.friendly.find(params[:startup_id])
+  end
 
   private
 
