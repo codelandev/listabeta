67aec3519f98200cb29d8fb289f1bc96a0a1a91f
Patrick Müller <patrick.e.muller@gmail.com>
2013-11-16 18:53:13 -0200

    Removed malfunction in questionnaire scope

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -17,7 +17,7 @@ class PagesController < ApplicationController
   def dashboard
     @feedbacks_total = Questionnaire.total_for(current_startup)
     @all_questionnaires = Questionnaire.where(startup: current_startup)
-    @count_last_seven_days_for = Questionnaire.last_seven_days(current_startup)
+    @count_last_seven_days_for = Questionnaire.where(startup: current_startup).group_by_day(:created_at).limit(7).count
     @a2 = Hash[Questionnaire.where(startup: current_startup).group('a2').count.map { |key, value| [A2::t(key), value] }]
     @a3 = Hash[Questionnaire.where(startup: current_startup).group('a3').count.map { |key, value| [A3::t(key), value] }]
     @a4 = Hash[Questionnaire.where(startup: current_startup).group('a4').count.map { |key, value| [A4::t(key), value] }]
