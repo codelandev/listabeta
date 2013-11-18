0adc45018fac53038a6b9a4ba800bcd381c901f6
Patrick Müller <patrick.e.muller@gmail.com>
2013-11-18 16:45:04 -0200

    Signup and redirect_to root after registration

--- app/controllers/startups/registrations_controller.rb
+++ app/controllers/startups/registrations_controller.rb
@@ -5,7 +5,7 @@ class Startups::RegistrationsController < Devise::RegistrationsController
     if resource.save
       if resource.active_for_authentication?
         set_flash_message :notice, :signed_up if is_navigational_format?
-        # sign_up(resource_name, resource)
+        sign_up(resource_name, resource)
         respond_with resource, :location => after_sign_up_path_for(resource)
       else
         set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_navigational_format?
@@ -21,6 +21,6 @@ class Startups::RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    dashboard_path
+    root_path
   end
 end
