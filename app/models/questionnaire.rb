0043a16f9160e24211432b3750ae903bb8be1f3b
Patrick Müller <patrick.e.muller@gmail.com>
2013-11-20 13:54:38 -0200

    Removed Presence true from opinion on questionnaires

--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -6,7 +6,7 @@ class Questionnaire < ActiveRecord::Base
   has_enumeration_for :a4
   has_enumeration_for :a5
 
-  validates :startup_id, :a1, :a2, :a3, :a4, :a5, :opinion, presence: true
+  validates :startup_id, :a1, :a2, :a3, :a4, :a5, presence: true
   validates :email, uniqueness: true, presence: true
 
   scope :total_for, ->(current_startup) { where(startup: current_startup).count }
