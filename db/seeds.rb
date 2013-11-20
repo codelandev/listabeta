190bfa8df4d57c66a6c0c4be83f677e4101d5cc5
Patrick Müller <patrick.e.muller@gmail.com>
2013-11-19 22:58:30 -0200

    Added more information on SEED

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,34 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-10.times do
-  Startup.create!([email: "test#{sn}@test.com",
-                  name: "Startup de teste #{sn}",
+10.times do |i| 
+  Startup.create!([email: "test#{i}@test.com",
+                  name: "Startup de teste #{i}",
                   website: "http://www.startup.com.br",
                   pitch: "Apenas mais um PITCH da startup",
                   description: "Apenas mais uma DESCRIÇÃO da startup",
-                  status: Status::IN_DEVELOPMENT,
+                  status: Phase::IN_DEVELOPMENT,
                   screenshot: File.open(Rails.root.join('spec', 'fixtures', 'photo.png')),
                   state: "Rio Grande do Sul",
                   city: "Porto Alegre",
-                  market_list: "Fitness#{sn}, Food#{sn}, Startups#{sn}",
+                  market_list: "Fitness#{i}, Food#{i}, Startups#{i}",
                   password: "123123123",
                   highlighted: false,
-                  approved: true])
+                  status: Status::APPROVED])
+end
+
+10.times do |i|
+  10.times do |j|
+    Questionnaire.create!([ 
+      startup: Startup.find(i+1), 
+      email: "feedback_#{j}#{i}@test.com", 
+      a1: "text", 
+      a2: A2::N, 
+      a3: A3::N, 
+      a4: A4::N, 
+      a5: A5::RANGE1, 
+      opinion: "text",
+      created_at: Time.now-(j+1).day
+    ])
+  end
 end
