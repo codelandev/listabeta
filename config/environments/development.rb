2027613f30cc77c0a5100899d789e3621fa30e07
Sérgio Schnorr Júnior <jr.schnorr@gmail.com>
2013-11-27 10:27:53 -0200

    Fix mailer host to listabeta.com.br

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ Listabeta::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Instead of send mailers open this on browser
+  config.action_mailer.default_url_options = { host: 'localhost:3000'}
   config.action_mailer.delivery_method = :letter_opener
 
   # In the development environment your application's code is reloaded on
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Listabeta::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # SendGrid default URL option
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
+  config.action_mailer.default_url_options = { host: 'listabeta.com.br'}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
