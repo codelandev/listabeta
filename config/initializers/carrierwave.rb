d647d43987c087e17ab8cecec899cdc543f5a19c
Patrick Edgar Müller <patrick.e.muller@gmail.com>
2015-04-30 11:05:09 -0300

    Removes wrong comma on carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,13 @@
 CarrierWave.configure do |config|
-  config.storage = Rails.env.production? ? :fog : :file
+  config.storage = (Rails.env.production? || Rails.env.staging?) ? :fog : :file
 
   config.fog_credentials = {
     provider:              'AWS',
-    aws_access_key_id:     Rails.application.secrets.aws_access_key,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_key,
+    aws_access_key_id:     Rails.application.secrets.aws_access_key.to_s,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_key.to_s
   }
 
-  config.fog_directory = Rails.application.secrets.aws_bucket_name
+  config.fog_directory = Rails.application.secrets.aws_bucket_name.to_s
   config.store_dir = "#{Rails.env}/uploads"
   config.enable_processing = false if Rails.env.test? or Rails.env.cucumber?
 end
