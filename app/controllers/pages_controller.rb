5fca3313540620dc7e3c7636e4ba212fe77b28c2
André Pinho <andresouzapinho@gmail.com>
2015-05-04 14:45:39 -0300

    Fixes home variable so there are only approved startups at home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   def home
     @all_markets = Startup.where(status: Status::APPROVED).tag_counts_on(:markets).order(:name).limit(20)
     @highlighteds = Startup.highlighteds.order_by_highlighted_at
-    @unhighlighteds = Startup.order_by_approves.limit(6)
+    @unhighlighteds = Startup.approvateds.order_by_approves.limit(6)
   end
 
   def markets
